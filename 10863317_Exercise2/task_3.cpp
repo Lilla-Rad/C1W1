a3dff917574c913993dbf07b37dfded417f5808a
Lilla-Rad <lilla.radek@students.plymouth.ac.uk>
2024-11-14 13:33:52 +0000

    Create task_3.cpp

---
+++ 10863317_Exercise2/task_3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int firstArray[5] = {5, 10, 15, 20, 25};
+	cout << firstArray[2]; // index starts at [0] so the 3rd value would be [2]
+  return 0;
+}
