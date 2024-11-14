1adbfc303b713084eafd05223ced56010f79b9a1
Lilla-Rad <lilla.radek@students.plymouth.ac.uk>
2024-11-14 13:22:08 +0000

    Create task_1.cpp

---
+++ 10863317_Exercise1/task_1.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // string is not needed for this excercise set
+using namespace std;
+
+int main() {
+	int numOne, numTwo;
+	numOne = 1;
+	numTwo = 2; // declare, then assign
+	cout << numOne + numTwo << endl; //print sum
+  return 0; // exit without returning anything
+}
