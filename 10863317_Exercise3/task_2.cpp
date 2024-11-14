d7d5404f55f14378df3b63937a77c62fddc19018
Lilla-Rad <lilla.radek@students.plymouth.ac.uk>
2024-11-14 13:52:33 +0000

    Update task_2.cpp

--- 10863317_Exercise3/task_2.cpp
+++ 10863317_Exercise3/task_2.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void staticLocalVarDemo() {
 	static int numberOfcalls = 0;
 	numberOfcalls++;
 	printf("Number of function calls: %d\n", numberOfcalls);
-  return 0;
+}
+
+int main() {
+	staticLocalVarDemo();
+	staticLocalVarDemo();
+	staticLocalVarDemo();
 }
