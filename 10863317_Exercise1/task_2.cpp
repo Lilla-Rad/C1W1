b758ba8ea17ab51bbf9d618de970ea188eb46fa7
Lilla-Rad <lilla.radek@students.plymouth.ac.uk>
2024-11-14 13:26:36 +0000

    Create task_2.cpp

---
+++ 10863317_Exercise1/task_2.cpp
@@ -0,0 +1,12 @@
+#include <iostream> // string is not needed for this excercise set
+using namespace std;
+
+void simpleArith(int num1, int num2) {
+	int product = num1 * num2;
+	cout << product << endl; // calculate product and then print
+}
+// void due to not returning any values
+
+int main() {
+  simpleArith(3,5);
+}
